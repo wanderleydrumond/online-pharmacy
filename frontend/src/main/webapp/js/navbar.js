27fe229c4b8748e6f568e577c8a6930dfb219a01
Wanderley Drumond <wanderley.drumond@gmail.com>
2023-05-01 15:29:37 +0100

    dashboard page created

--- frontend/src/main/webapp/js/navbar.js
+++ frontend/src/main/webapp/js/navbar.js
@@ -1,40 +1,46 @@
-let searchForm = document.querySelector('.search-form');
-document.querySelector('#search-btn').onclick = () => {
-	searchForm.classList.toggle('active');
-	shoppingCart.classList.remove('active');
-	loginForm.classList.remove('active');
-	navbar.classList.remove('active');
-}
+let searchForm = document.querySelector(".search-form");
+document.querySelector("#search-btn").onclick = () => {
+	searchForm.classList.toggle("active");
+	shoppingCart.classList.remove("active");
+	loginForm.classList.remove("active");
+	navbar.classList.remove("active");
+};
 
-let shoppingCart = document.querySelector('.shopping-cart');
-document.querySelector('#cart-btn').onclick = () => {
-	searchForm.classList.remove('active');
-	shoppingCart.classList.toggle('active');
-	loginForm.classList.remove('active');
-	navbar.classList.remove('active');
-}
+let shoppingCart = document.querySelector(".shopping-cart");
+document.querySelector("#cart-btn").onclick = () => {
+	searchForm.classList.remove("active");
+	shoppingCart.classList.toggle("active");
+	loginForm.classList.remove("active");
+	navbar.classList.remove("active");
+};
 
-let loginForm = document.querySelector('.login-form');
-document.querySelector('#login-btn').onclick = () => {
-	searchForm.classList.remove('active');
-	shoppingCart.classList.remove('active');
-	loginForm.classList.toggle('active');
-	navbar.classList.remove('active');
-}
+let loginForm = document.querySelector(".login-form");
+document.querySelector("#login-btn").onclick = () => {
+	searchForm.classList.remove("active");
+	shoppingCart.classList.remove("active");
+	loginForm.classList.toggle("active");
+	navbar.classList.remove("active");
+};
 
-let navbar = document.querySelector('.navbar');
-document.querySelector('#menu-btn').onclick = () => {
-	searchForm.classList.remove('active');
-	shoppingCart.classList.remove('active');
-	loginForm.classList.remove('active');
-	navbar.classList.toggle('active');
-}
+let navbar = document.querySelector(".navbar");
+document.querySelector("#menu-btn").onclick = () => {
+	searchForm.classList.remove("active");
+	shoppingCart.classList.remove("active");
+	loginForm.classList.remove("active");
+	navbar.classList.toggle("active");
+};
+
+let dashboardBtn = document.getElementById("dashboard-btn");
+dashboardBtn.addEventListener("click", function () {
+	window.location.href = "dashboard.html";
+});
+
 window.onscroll = () => {
-	searchForm.classList.remove('active');
-	shoppingCart.classList.remove('active');
-	loginForm.classList.remove('active');
-	navbar.classList.remove('active');
-}
+	searchForm.classList.remove("active");
+	shoppingCart.classList.remove("active");
+	loginForm.classList.remove("active");
+	navbar.classList.remove("active");
+};
 
 var swiper = new Swiper(".product-slider", {
 	loop: true,
@@ -44,19 +50,19 @@ var swiper = new Swiper(".product-slider", {
 		disableOnInteraction: false,
 	},
 	breakpoints: {
-	  0: {
-		slidesPerView: 1,
-	  },
-	  768: {
-		slidesPerView: 2,
-	  },
-	  1020: {
-		slidesPerView: 3,
-	  },
+		0: {
+			slidesPerView: 1,
+		},
+		768: {
+			slidesPerView: 2,
+		},
+		1020: {
+			slidesPerView: 3,
+		},
 	},
-  });
+});
 
-  var swiper = new Swiper(".review-slider", {
+var swiper = new Swiper(".review-slider", {
 	loop: true,
 	spaceBetween: 20,
 	autoplay: {
@@ -64,14 +70,14 @@ var swiper = new Swiper(".product-slider", {
 		disableOnInteraction: false,
 	},
 	breakpoints: {
-	  0: {
-		slidesPerView: 1,
-	  },
-	  768: {
-		slidesPerView: 2,
-	  },
-	  1020: {
-		slidesPerView: 3,
-	  },
+		0: {
+			slidesPerView: 1,
+		},
+		768: {
+			slidesPerView: 2,
+		},
+		1020: {
+			slidesPerView: 3,
+		},
 	},
-  });
+});
